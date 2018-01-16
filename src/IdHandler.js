ff24ef7dcd715f1e4de6843ad85798c81a46c5ce
sridevs <senthilnathansridev@gmail.com>
2018-01-16 07:38:17 +0530

    added increment and reset features in idHandler class

--- src/IdHandler.js
+++ src/IdHandler.js
@@ -1,8 +1,16 @@
 class IdHandler {
   constructor(initialValue, addend) {
-    this.id = initialValue || 0;
+    this.initialValue = initialValue || 0;
+    this.id = this.initialValue;
     this.addend = addend || 1;
   }
 
+  increment() {
+    this.id += this.addend;
+  }
+
+  reset() {
+    this.id = this.initialValue;
+  }
 }
 exports.IdHandler = IdHandler;
--- test/testIdHandler.js
+++ test/testIdHandler.js
@@ -5,11 +5,34 @@ const assert = require('chai').assert;
 describe('class IdHandler',function () {
   beforeEach(function () {
     idHandler = new IdHandler;
+    idHandler2 = new IdHandler(1,2);
   })
   it('creates an instanceof itself',function () {
     assert.isTrue(idHandler instanceof IdHandler);
   })
   describe('increment',function () {
-    it('it increments itself by ')
+    it('increments itself by 1',function () {
+      idHandler.increment();
+      assert.equal(idHandler.id,1);
+      idHandler.increment();
+      assert.equal(idHandler.id,2);
+      idHandler.increment();
+      assert.equal(idHandler.id,3);
+    })
+
+    it('increments itself by given addend',function () {
+      idHandler2.increment()
+      assert.equal(idHandler2.id,3);
+      idHandler2.increment();
+      assert.equal(idHandler2.id,5);
+    })
+    describe('reset',function () {
+      it('resets its id to initialValue',function () {
+        idHandler2.increment();
+        assert.equal(idHandler2.id,3);
+        idHandler2.reset();
+        assert.equal(idHandler2.id,1);
+      })
+    })
   })
 })
